fc737d833769f8c02d3048cd8822a7882efcf5b9
Mac <1785193871@qq.com>
2025-06-15 23:28:52 +0800

    test

--- LoginForm.cs
+++ LoginForm.cs
@@ -62,12 +62,12 @@ namespace GasFormsApp
             InitializeComponent();
             this.Load += LoginForm_Load;
 
-            MainForm main = new MainForm(false);
-            this.Hide();
+            //MainForm main = new MainForm(false);
+            //this.Hide();
 
-            main.ShowDialog();
+            //main.ShowDialog();
 
-            this.Close();
+            //this.Close();
         }
 
         //private int GetFailedLoginCount()
