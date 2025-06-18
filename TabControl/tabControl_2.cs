16c03925751ad99c9fcc361548756f80597b17d6
公司电脑1 <1@qq.com>
2025-06-18 13:52:59 +0800

    修改了曲线图

--- TabControl/tabControl_2.cs
+++ TabControl/tabControl_2.cs
@@ -265,8 +265,8 @@ namespace GasFormsApp.TabControl
                     try
                     {
                         var pythonPath = @"Python_embed\python.exe"; // 嵌入式解释器路径
-                        var scriptPath = @"Python_embed\Python\aaa.cpython-312.pyc";           // 你实际的 .py 文件路径
-                        //var scriptPath = @"Python_embed\Python\aaa.py";
+                        //var scriptPath = @"Python_embed\Python\aaa.cpython-312.pyc";           // 你实际的 .py 文件路径
+                        var scriptPath = @"Python_embed\Python\aaa.py";
 
                         ProcessStartInfo psi = new ProcessStartInfo
                         {
